0473344afdf61f7386fee802cde8f64e3e95ce8a
SandraMM1054 <sandramendez@mail.webwe.edu>
2025-10-23 19:00:35 -0600

    tarea mi lista

---
+++ M9_Presencial/lista.py
@@ -0,0 +1,21 @@
+# parte 1 
+
+milista = ["manzana", "pera", "banano", "naranja"]
+print(milista)
+
+# parte 2
+
+# le mostre bucle
+
+# parte 3 diccionario
+
+estediccionario = {
+  "marca": "Toyota",
+  "modelo": "4Runner",
+  "year": 2020
+}
+print(estediccionario)
+
+# parte 4 tupla
+
+mitupla = tuple(("manzana", "limon", "banano")) # doble parentesisprint(mitupla)
